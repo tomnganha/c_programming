2819d2c9f77c60a416db9b48f9d02a34aab68646
tomnganha <hoangthai050302@gmail.com>
2024-03-28 20:04:52 +0700

    27_3_2024

--- contest5/contest5/contest5.c
+++ contest5/contest5/contest5.c
@@ -1,1 +1,32 @@
-asdas
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+//BAI 1
+//int nt(int n) {
+//	if (n == 2) return 1;
+//	if (n < 2) return 0;
+//	if (n > 2) {
+//		for (int i = 2; i < sqrt(n); i++) {
+//			if (n % i == 0) {
+//				return 1;
+//			}
+//		}
+//	}
+//}
+
+
+
+int main() {
+	int n;
+	int A[1000];
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &A[i]);
+	}
+	for (int i = 0; i < n; i = i + 2) {
+		if (A[i] % 2 == 0) printf("%d ", A[i]);
+	}
+	
+	
+	return 0;
+}
