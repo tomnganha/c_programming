4e620fdb915c6815444427fbe64b79a80b06a2ac
tomnganha <hoangthai050302@gmail.com>
2024-03-28 23:42:13 +0700

    video57

--- contest5/contest5/contest5.c
+++ contest5/contest5/contest5.c
@@ -14,19 +14,89 @@
 //	}
 //}
 
+//Bai 11
 
 
+
+//int check(int n) {
+//	for (int i = 0; i < n; i++) {
+//		if (F[i] == n) return 1;
+//	}
+//	return 0;
+//}
+//void find(int a[], int n) {
+//	int min=a[0];
+//	int max=a[0];
+//	int lo_min=0;
+//	int lo_max=0;
+//	for (int i = 0; i < n; i++) {
+//		if (a[i] > max) {
+//			max = a[i];
+//			lo_max == i;
+//		}
+//		if (a[i] <= min) {
+//			min = a[i];
+//			lo_min = i;
+//		};
+//	}
+//	printf("%d %d", lo_min, lo_max);
+//}
+
+//ll gcd(long long a, long long b) {
+//	if (b == 0) {
+//		return a;
+//	}
+//	else {
+//		return gcd(b, a % b);
+//	}
+//}
+//int nt(int n) {
+//	if (n < 2) return 0;
+//	if (n == 2) return 1;
+//	for (int i = 2; i <= sqrt(n); i++) {
+//		if (n % i == 0) return 0;
+//	}
+//	return 1;
+//}
+//int thuan_nghich(int n) {
+//	int temp = 0;
+//	int temp1 = n;
+//	while (n != 0)
+//	{
+//		temp = temp * 10 + n % 10; n /= 10;
+//	}
+//	return temp == temp1;
+//}
+//int chinh_phuong(int n) {
+//	return (sqrt(n) * sqrt(n)) == n;
+//}
+//
+//int yc4(int n) {
+//	int sum = 0;
+//	while (n!=0)
+//	{
+//		sum += n % 10; n /= 10;
+//	}
+//	return nt(sum);
+//}
+
+int dem[1000001];
 int main() {
 	int n;
-	int A[1000];
 	scanf("%d", &n);
+	int a[100];
 	for (int i = 0; i < n; i++) {
-		scanf("%d", &A[i]);
+		scanf("%d", &a[i]);
 	}
-	for (int i = 0; i < n; i = i + 2) {
-		if (A[i] % 2 == 0) printf("%d ", A[i]);
+	for (int i = 0; i < n; i++) {
+		dem[a[i]]++;
 	}
-	
+	int count=0;
+	for (int i = 1000001; i >=0; i--) {
+		if (dem[i] >= count) count = i;
+	}
+	printf("%d", count);
+
 	
 	return 0;
 }
