3e02d2ba78a28738540971ea35616f6d5afb0d60
tomnganha <hoangthai050302@gmail.com>
2024-03-31 18:53:42 +0700

    contest6_video62

--- contest6/contest6/contest6.c
+++ contest6/contest6/contest6.c
@@ -32,11 +32,48 @@
 //}
 
 //Sap xep chen: insert sort
-void insert_sort(int a[], int n) {
-	for (int i = 1; i < n; i++) {
-		for (int j = 0; j<i; j++) {
-			if (a[i] <a[j]) {
-				int temp = a[i]; a[i] = a[j]; a[j] = temp;
+//void insert_sort(int a[], int n) {
+//	for (int i = 1; i < n; i++) {
+//		for (int j = 0; j<i; j++) {
+//			if (a[i] <a[j]) {
+//				int temp = a[i]; a[i] = a[j]; a[j] = temp;
+//				
+//			}
+//		}
+//	}
+//}
+//int main() {
+//	int n;
+//	scanf("%d", &n);
+//	int a[100];
+//		for (int i = 0; i < n; i++) {
+//		scanf("%d", &a[i]);
+//	}
+//	for (int i = 0; i < n; i++) {
+//		printf("%d ", a[i]);
+//	}
+//	printf("\n");
+//	insert_sort(a, n);
+//	for (int i = 0; i < n; i++) {
+//		printf("%d ", a[i]);
+//	}
+//	return 0;
+//}
+
+//SAP XEP dem phan phoi
+void couting_sort(int a[], int n) {
+	int dem[100001];
+	for (int i = 0; i < 100001; i++) {
+		dem[i] = 0;
+	}
+	for (int i = 0; i < n; i++) {
+		dem[a[i]]++;
+	}
+	
+	for (int i = 0; i < 100001; i++) {
+		if (dem[i] != 0) {
+			for (int j = 1; j <= dem[i]; j++) {
+				printf("%d ", i);
 			}
 		}
 	}
@@ -45,16 +82,12 @@ int main() {
 	int n;
 	scanf("%d", &n);
 	int a[100];
-		for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) {
 		scanf("%d", &a[i]);
 	}
 	for (int i = 0; i < n; i++) {
 		printf("%d ", a[i]);
 	}
 	printf("\n");
-	insert_sort(a, n);
-	for (int i = 0; i < n; i++) {
-		printf("%d ", a[i]);
-	}
-	return 0;
+	couting_sort(a, n);
 }
