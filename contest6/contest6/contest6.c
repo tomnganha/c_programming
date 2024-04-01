01f706135e07ee1c13fdf00e7b488e55ad6ec87d
tomnganha <hoangthai050302@gmail.com>
2024-04-01 23:28:35 +0700

    conetst 6 end

--- contest6/contest6/contest6.c
+++ contest6/contest6/contest6.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<math.h>
+#include<stdlib.h>
 //billed sort
 //void bulled_sort(int a[], int n) {
 //	for (int i = 0; i < n  - 1; i++) {
@@ -181,28 +182,88 @@
 //	return res;
 //}
 //tim phan tu cuoi cung trong day tang dan nho hon hoac bang x
-int last(int a[], int n, int x) {
-	int l = 0, r = 0;
-	int mid, res=-1;
-	while (l <= r) {
-		mid = (l+r)/2;
-		if (a[mid] <= x) {
-			res = mid;
-			l = mid + 1;
-		}
-		else {
-			r = mid - 1;
-		}
+//int last(int a[], int n, int x) {
+//	int l = 0, r = 0;
+//	int mid, res=-1;
+//	while (l <= r) {
+//		mid = (l+r)/2;
+//		if (a[mid] <= x) {
+//			res = mid;
+//			l = mid + 1;
+//		}
+//		else {
+//			r = mid - 1;
+//		}
+//	}
+//	return res;
+//}
+//int main() {
+//	int n, k;
+//	scanf("%d%d", &n, &k);
+//	int a[100];
+//	for (int i = 0; i < n; i++) {
+//		scanf("%d", &a[i]);
+//	}
+//	printf("%d", last(a, n, k));
+//	return 0;
+//}
+
+//int prime[1000000];
+//void sang() {
+//	for (int i = 0; i < 1000000; i++) {
+//		prime[i] = 1;
+//
+//	}
+//	prime[0] = prime[1] = 0;
+//	for (int i = 2; i < 1000000; i++) {
+//		if (prime[i]) {
+//			for (int j = i * i; i < 1000000; j += i) {
+//				prime[j] = 0;
+//			}
+//		}
+//	}
+//	
+//}
+//int main() {
+//	sang();
+//	int t;
+//	scanf("%d", &t);
+//	while (t--)
+//	{
+//		int n; scanf("%d", &n);
+//		if (prime[n]) {
+//			printf("YES\n");
+//		}
+//		else {
+//			printf("NO\n");
+//		}
+//	}
+//	return 0;
+//}
+
+//Su dung ham quitsort va xay dung comparator
+
+int comp(const void* a, const void* b) {
+	int* x = (int*)a;
+	int* y = (int*)b;
+	/*if (*x < *y) {
+		return -1;
 	}
-	return res;
+	else {
+		return 1;
+	}*/
+	return *y - *x;
 }
 int main() {
-	int n, k;
-	scanf("%d%d", &n, &k);
+	int n;
+	scanf("%d", &n);
 	int a[100];
 	for (int i = 0; i < n; i++) {
-		scanf("%d", &a[i]);
+		scanf("%d" ,&a[i]);
 	}
-	printf("%d", last(a, n, k));
+	qsort(a, n, sizeof(int), comp);
+	for (int i = 0; i < n; i++) {
+		printf("%d ", a[i]);
+	}
 	return 0;
 }
