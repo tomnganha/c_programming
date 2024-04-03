e42945005956c6959ebc80682e7dbaae2e814f0f
tomnganha <hoangthai050302@gmail.com>
2024-04-03 22:15:24 +0700

    video81

--- contest8/contest8/contest8.c
+++ contest8/contest8/contest8.c
@@ -4,20 +4,20 @@
 #include<stdio.h>
 #include<math.h>
 
-int sum_hang(int a[][100], int n, int m, int pos) {
-	int sum = 0;
-	for (int j = 0; j < m; j++) {
-		sum += a[pos][j];
-	}
-	return sum;
-}
-int sum_cot(int a[][100], int n, int m, int pos) {
-	int sum = 0;
-	for (int i = 0; i < n; i++) {
-		sum += a[i][pos];
-	}
-	return sum;
-}
+//int sum_hang(int a[][100], int n, int m, int pos) {
+//	int sum = 0;
+//	for (int j = 0; j < m; j++) {
+//		sum += a[pos][j];
+//	}
+//	return sum;
+//}
+//int sum_cot(int a[][100], int n, int m, int pos) {
+//	int sum = 0;
+//	for (int i = 0; i < n; i++) {
+//		sum += a[i][pos];
+//	}
+//	return sum;
+//}
 void nhap(int a[][100], int n, int m) {
 	for (int i = 0; i < n; i++) {
 		for(int j = 0;j < m; j++){
@@ -33,27 +33,30 @@ void xuat(int a[][100], int n, int m) {
 		printf("\n");
 	}
 }
+int nt(int n) {
+	if (n == 2) return 1;
+	if (n < 2) return 0;
+	for (int i = 2; i <= sqrt(n); i++) {
+		if (n % i == 0) 
+			return 0;
+	}
+	return 1;
+}
 int a[100][100];
 int main() {
 	int n, m;
 	scanf("%d%d", &n, &m);
-	
 	nhap(a, n, m);
 	xuat(a, n, m);
-	int max = a[0][0], min = a[0][0];
+	
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			if (max < a[i][j]) {
-				max = a[i][j];
+			if (nt(a[i][j])) {
+				printf("%d ", a[i][j]);
 			}
-			if (min > a[i][j]) {
-				min = a[i][j];
-			}
-			
 		}
+		printf("\n");
 	}
-	printf("\nmax= %d, min=%d", max, min);
-	printf("\nsum hang 1 %d", sum_hang(a, n, m, 1));
-	printf("\nsum cot 1 %d", sum_cot(a, n, m, 1));
+	
 	return 0;
 }
